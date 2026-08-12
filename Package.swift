fc2984994dc4c995d92f2c2e2eea8e07e233e278
mtecfix <116046821+mtecfix@users.noreply.github.com>
2026-08-12 12:12:21 -0400

    iOS only platform, remove macOS

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "FinanceApp",
-    platforms: [.iOS(.v16), .macOS(.v13)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(name: "FinanceApp", targets: ["FinanceApp"])
     ],
