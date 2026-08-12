f7003271d2c5bca4c485db6d944329516acddbed
mtecfix <116046821+mtecfix@users.noreply.github.com>
2026-08-11 21:46:06 -0400

    Add source files

---
+++ FinanceApp/Config.swift
@@ -0,0 +1,7 @@
+import Foundation
+enum Config {
+    static let apiEndpoint   = "https://uiv731ay0h.execute-api.us-east-1.amazonaws.com/dev"
+    static let cognitoPoolId = "us-east-1_Z3zNNwEQG"
+    static let cognitoClient = "2h50ha0j15j34n57p0gjpaqttp"
+    static let awsRegion     = "us-east-1"
+}
